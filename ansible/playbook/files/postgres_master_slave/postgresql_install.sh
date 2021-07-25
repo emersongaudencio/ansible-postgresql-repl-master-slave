07e60a6de5c533f6b940acab720e1dc3c64760ce
Emerson Gaudencio <emerson.fender@gmail.com>
2021-07-25 15:06:47 +0100

    added support for CentOS 8

--- ansible/playbook/files/postgres_master_slave/postgresql_install.sh
+++ ansible/playbook/files/postgres_master_slave/postgresql_install.sh
@@ -7,6 +7,54 @@ echo "$2" > /tmp/SERVERID
 echo "$3" > /tmp/PRIMARY_SERVER
 PG_VERSION=$(cat /tmp/PG_VERSION)
 
+# os_type = rhel
+os_type=
+# os_version as demanded by the OS (codename, major release, etc.)
+os_version=
+supported="Only RHEL/CentOS 7 & 8 are supported for this installation process."
+
+msg(){
+    type=$1 #${1^^}
+    shift
+    printf "[$type] %s\n" "$@" >&2
+}
+
+error(){
+    msg error "$@"
+    exit 1
+}
+
+identify_os(){
+    arch=$(uname -m)
+    # Check for RHEL/CentOS, Fedora, etc.
+    if command -v rpm >/dev/null && [[ -e /etc/redhat-release || -e /etc/os-release ]]
+    then
+        os_type=rhel
+        el_version=$(rpm -qa '(oraclelinux|sl|redhat|centos|fedora|rocky|alma|system)*release(|-server)' --queryformat '%{VERSION}')
+        case $el_version in
+            1*) os_version=6 ; error "RHEL/CentOS 6 is no longer supported" "$supported" ;;
+            2*) os_version=7 ;;
+            5*) os_version=5 ; error "RHEL/CentOS 5 is no longer supported" "$supported" ;;
+            6*) os_version=6 ; error "RHEL/CentOS 6 is no longer supported" "$supported" ;;
+            7*) os_version=7 ;;
+            8*) os_version=8 ;;
+             *) error "Detected RHEL or compatible but version ($el_version) is not supported." "$supported"  "$otherplatforms" ;;
+         esac
+         if [[ $arch == aarch64 ]] && [[ $os_version != 7 ]]; then error "Only RHEL/CentOS 7 are supported for ARM64. Detected version: '$os_version'"; fi
+    fi
+
+    if ! [[ $os_type ]] || ! [[ $os_version ]]
+    then
+        error "Could not identify OS type or version." "$supported"
+    fi
+}
+
+### OS auto discovey to identify which is the OS and version thats been used it.
+identify_os
+
+echo $os_type
+echo $os_version
+
 ##### FIREWALLD DISABLE ########################
 systemctl disable firewalld
 systemctl stop firewalld
@@ -16,34 +64,49 @@ sed -ie 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 /usr/sbin/setenforce 0
 
 ### clean yum cache ###
-yum clean headers
-yum clean packages
-yum clean metadata
+yum clean all
 
 ####### PACKAGES ###########################
-# -------------- For RHEL/CentOS 7 --------------
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+if [[ $os_type == "rhel" ]]; then
+    if [[ $os_version == "7" ]]; then
+      # -------------- For RHEL/CentOS 7 --------------
+      yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+      yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+    elif [[ $os_version == "8" ]]; then
+      # -------------- For RHEL/CentOS 8 --------------
+      yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+      yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+      sed -ie 's/enabled=1/enabled=1\nmodule_hotfixes=1/g' /etc/yum.repos.d/pgdg-redhat-all.repo
+    fi
 # yum -y install epel-release
+fi
 
 ### remove old packages ####
 yum -y remove postgres
 yum -y remove 'postgres*'
 
-### install pre-packages ####
-yum -y install screen expect nload bmon iptraf glances perl perl-DBI openssl pigz zlib file sudo  libaio rsync snappy net-tools wget nmap htop dstat sysstat perl-IO-Socket-SSL perl-Digest-MD5 perl-TermReadKey socat libev gcc zlib zlib-devel openssl openssl-devel python-pip python-devel zip
+### clean yum cache ###
+yum clean all
 
-#### REPO Pg ######
-# -------------- For RHEL/CentOS 7 --------------
-yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+### monitoring pre-packages ####
+yum -y install nload bmon iptraf glances nmap htop dstat sysstat socat
+
+# dev tools
+yum -y install screen yum-utils expect perl perl-DBI perl-IO-Socket-SSL perl-Digest-MD5 perl-TermReadKey  libev gcc zlib zlib-devel openssl openssl-devel python3 python3-pip python3-devel
+
+# others pre-packages
+yum -y install pigz zlib file sudo libaio rsync snappy net-tools wget
 
 ### clean yum cache ###
-yum clean headers
-yum clean packages
-yum clean metadata
+yum clean all
 
 ### installation of Posgresql via yum ####
 yum -y install postgresql$PG_VERSION postgresql$PG_VERSION-server
-yum -y install perl-DBD-Pg postgresql$PG_VERSION-python postgresql$PG_VERSION-contrib
+yum -y install postgresql$PG_VERSION-plpython3
+yum -y install postgresql$PG_VERSION-pltcl
+yum -y install postgresql$PG_VERSION-contrib
+yum -y install postgresql$PG_VERSION-odbc
+yum -y install perl-DBD-Pg
 
 ### Percona #####
 ### https://www.percona.com/doc/percona-server/LATEST/installation/yum_repo.html
@@ -122,9 +185,13 @@ sysctl -p
 
 #####  Pg LIMITS ###########################
 mkdir -p /etc/systemd/system/postgresql.service.d/
-echo '[Service]' > /etc/systemd/system/postgresql.service.d/limits.conf
+echo ' ' > /etc/systemd/system/postgresql.service.d/limits.conf
+echo '# postgres' >> /etc/systemd/system/postgresql.service.d/limits.conf
+echo '[Service]' >> /etc/systemd/system/postgresql.service.d/limits.conf
 echo 'LimitNOFILE=102400' >> /etc/systemd/system/postgresql.service.d/limits.conf
-echo '[Service]' > /etc/systemd/system/postgresql.service.d/timeout.conf
+echo ' ' > /etc/systemd/system/postgresql.service.d/timeout.conf
+echo '# postgres' >> /etc/systemd/system/postgresql.service.d/timeout.conf
+echo '[Service]' >> /etc/systemd/system/postgresql.service.d/timeout.conf
 echo 'TimeoutSec=28800' >> /etc/systemd/system/postgresql.service.d/timeout.conf
 systemctl daemon-reload
 
